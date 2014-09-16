b39f6a51b28f23e0d0c06e7338ed74b4eedb1485
Reed Nelson-Saunders <reedns@hotmail.com>
2014-09-16 16:28:22 -0700

    refactored tests so they are DRY

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,20 +3,21 @@ require "minitest/spec"
 require "card.rb"
 
 describe "Card" do
+  before do
+    @card = Card.new("Ace", "Spades")
+  end
+
   it "has a rank" do
-    card = Card.new("Ace", "Spades")
-    card.rank.must_equal("Ace")
+    @card.rank.must_equal("Ace")
   end
 
   it "has a suit" do
-    card = Card.new("Ace", "Spades")
-    card.suit.must_equal("Spades")
+    @card.suit.must_equal("Spades")
   end
 
   describe "#to_pretty_s" do
     it "writes out string" do
-      card = Card.new("Ace", "Spades")
-      card.to_pretty_s.must_equal("Ace of Spades")
+      @card.to_pretty_s.must_equal("Ace of Spades")
     end
   end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,14 +4,16 @@ require "deck.rb"
 
 
 describe "Deck" do
+  before do
+    @deck = Deck.new
+  end
+
   it "has an array of card objects" do
-    deck = Deck.new
     deck.cards.must_be_instance_of Array
     deck.cards.pop.must_be_instance_of Card
   end
 
   it "has 52 cards" do
-    deck = Deck.new
     deck.cards.size.must_equal(52)
   end
 end
